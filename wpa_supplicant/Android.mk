7ba9d4a95b01adc602f1f5a2391dcd7fd8281915
Michael Bestas <mkbestas@lineageos.org>
2024-06-15 22:21:49 +0300

    Revert "set LOCAL_MIN_SDK_VERSION as 34"
    Reason for revert: Not present in QPR3.
    
    This reverts commit 7ca3b7ab9f215ada0f11e8d0d6a1c68bfc5f8354.
    
    Change-Id: I3da519edc8a3294d6e71ca0f861ce9009eb240bf

--- wpa_supplicant/Android.mk
+++ wpa_supplicant/Android.mk
@@ -1977,7 +1977,6 @@ LOCAL_SHARED_LIBRARIES += libutils libbase
 LOCAL_SHARED_LIBRARIES += libbinder_ndk
 LOCAL_STATIC_LIBRARIES += libwpa_aidl
 LOCAL_VINTF_FRAGMENTS := aidl/android.hardware.wifi.supplicant.xml
-LOCAL_MIN_SDK_VERSION := 34
 ifeq ($(WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY), true)
 LOCAL_INIT_RC=aidl/android.hardware.wifi.supplicant-service.rc
 endif
@@ -2083,7 +2082,6 @@ LOCAL_SHARED_LIBRARIES := \
     libutils \
     liblog \
     libssl
-LOCAL_MIN_SDK_VERSION := 34
 LOCAL_EXPORT_C_INCLUDE_DIRS := \
     $(LOCAL_PATH)/aidl
 include $(BUILD_STATIC_LIBRARY)
